4ac4dcfb7070485f251d6205970c6656872b03cb
Emiliano Javier González <ejgonzalez90@hotmail.com>
2019-04-03 23:13:24 -0300

    Fixed .UseUrls to use HTTPS

--- CV.People/Program.cs
+++ CV.People/Program.cs
@@ -19,7 +19,7 @@ namespace CV.People
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:6003/")
+                .UseUrls("https://localhost:6003/")
                 .UseStartup<Startup>();
     }
 }
